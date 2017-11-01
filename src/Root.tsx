44f78422d9f1d8ed454f7ddc224981094c4d7dc9
Sebastián Gaviria <vagyriag@hotmail.com>
2017-11-01 12:38:40 -0500

    localhost

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export class Root extends React.Component<{},{}>{
     render(){
-        return <div>
+        return <div>  
             Hola desde React
         </div>
     }
