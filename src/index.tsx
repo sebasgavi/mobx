c12a7ef6668f73ffd0ad265c2ca1730df30f8692
Sebastian Gaviria Giraldo <webmaster@colegiobolivar.edu.co>
2018-05-29 11:47:08 -0500

    polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
+
 import * as React from 'react';
 import { render } from 'react-dom';
 
-<<<<<<< HEAD
-class Test extends React.Component<{},{}>{
-  render(){
-    return <div>
-      React is working!
-    </div>
-  }
-}
-=======
 import { Root } from './root';
->>>>>>> fb933db6c0786ee4967cf279b05d02bd0e1e2f03
 
 render(
   <Root />,
